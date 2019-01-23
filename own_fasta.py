81598bd85ed06099f84420c82719776741269b91
agentx115 <r_dowling@live.co.uk>
2019-01-23 10:09:16 +0000

    fixed reverse strand

--- own_fasta.py
+++ own_fasta.py
@@ -4,13 +4,15 @@ Created on Tue Jan 22 15:25:31 2019
 
 @author: Rhiannon
 """
-
+#%%
 from Bio import SeqIO
+#from Bio.Seq import Seq
+#%%
 
 #read in sequence as fasta
 fname = "sequence.txt"
 dna = str(SeqIO.read(fname, "fasta").seq)
-
+#%%
 #create codons list
 
 codons = {}
@@ -23,17 +25,24 @@ codon_list = codon_content.split("\n")
 
 for i in range(0, len(codon_list)-1, 2):
     codons[codon_list[i]] = codon_list[i+1]
-    
+#%%
 #get reverse strand
-    
-reverse_dna = dna
-reverse_dna = reverse_dna.replace("a", "x")
+#own way:
+reverse_dna = dna.replace("a", "x")
 reverse_dna = reverse_dna.replace("t", "a")
 reverse_dna = reverse_dna.replace("x", "t")
 reverse_dna = reverse_dna.replace("c", "d")
 reverse_dna = reverse_dna.replace("g", "c")
 reverse_dna = reverse_dna.replace("d", "g")
+reverse_dna = reverse_dna[::-1]
 
+#BioPy ver:
+#much shorter!!
+#dna1 = Seq(dna)
+#reverse_dna = dna1.reverse_complement()
+
+    
+ #%%   
 #translate sequence
 
 def translate_seq(dna_seq):
@@ -45,7 +54,8 @@ def translate_seq(dna_seq):
         else:
             trans = trans + "-"
     return trans
-    
+
+#%%
 #get reading frames
 
 rf_list = []
